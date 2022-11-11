09c89b778d83c4cc7099970aa89ab934ded8bfc0
sachintanwarbookeeapp <111785484+sachintanwarbookeeapp@users.noreply.github.com>
2022-11-11 13:39:25 +0530

    That I am changing the things

--- src/main/java/webPagesLocators/testingJava.java
+++ src/main/java/webPagesLocators/testingJava.java
@@ -3,6 +3,6 @@ package webPagesLocators;
 public class testingJava {
     public static void main(String[] args)
     {
-        System.out.println(" Hree I am testing that it is good");
+
     }
 }

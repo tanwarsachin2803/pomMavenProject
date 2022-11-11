c9e99f49fdf70e3e5d6a0e5e4071e21d7f0fbc04
sachintanwarbookeeapp <111785484+sachintanwarbookeeapp@users.noreply.github.com>
2022-11-11 13:34:48 +0530

    That I am changing the things

---
+++ src/main/java/webPagesLocators/testingJava.java
@@ -0,0 +1,8 @@
+package webPagesLocators;
+
+public class testingJava {
+    public static void main(String[] args)
+    {
+        System.out.println(" Hree I am testing that it is good");
+    }
+}
